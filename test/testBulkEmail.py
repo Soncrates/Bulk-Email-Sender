ffb4b418bb0d538552db9a7731ddf567d61c22e3
Soncrates <emersoncus@yahoo.com>
2022-02-13 17:40:46 -0500

    Update testBulkEmail.py

--- test/testBulkEmail.py
+++ test/testBulkEmail.py
@@ -4,20 +4,20 @@ path.append("../")
 import libCommon as COMMON
 import logging as log
 import unittest
-import create_team_mappings as TEST
+import script as TEST
 
-def load_response(filename) :
-    filename_list = COMMON.find_files(filename)
-    with open(filename_list[0]) as fp :
-         return "".join(fp.readlines())
-class TestScraperFunctions(unittest.TestCase) :
-    team_list_MLA = None
-    def setUp(self) : 
+class TestEmailSend(unittest.TestCase) :
+    def dep_setUp(self) : 
         self.team_list_MLA = load_response("./test_input/team_list_MLA.html")
-    def testLacrosseStep02(self) :
-        obj = [ TEST.step01_parse_response(team_list) for team_list in [self.team_list_MLA] ]
-        obj = TEST.merge_divisions(*obj)
-        log.debug(COMMON.pretty_print(obj))
+    def testshowMessage(self) :
+        obj = TEST.CLIENTS.personalize(user='Bob',name="Bobby")
+        log.info(obj)
+    def testBulkSend(self) :
+        SenderAddress = "marketing.womenonwheels@gmail.com"
+        password = "HotBikerBabes@2020"
+        server = TEST.EMAIL.gmail(user=SenderAddress,pswd=password)
+        client_list = TEST.CLIENTS.load()
+        TEST.EMAIL.bulk(server,*client_list)
 
 if __name__ == '__main__' :
    import sys
